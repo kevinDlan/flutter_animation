9254b7ddd5cb250f0e8569aac00ef0736d2c5970
kevinDlan <kevinkone19@gmail.com>
2020-11-10 23:25:02 +0000

    original projet

---
+++ android/app/src/main/java/kevin/flutter_animation/MainActivity.java
@@ -0,0 +1,6 @@
+package kevin.flutter_animation;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/screens/home.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animation/shared/screenTitle.dart';
+import 'package:flutter_animation/shared/tripList.dart';
+
+class Home extends StatefulWidget {
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+            padding: EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                  image: AssetImage("images/bg.png"),
+                  fit: BoxFit.fitWidth,
+                  alignment: Alignment.topLeft
+              ),
+            ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                SizedBox(height: 30),
+                SizedBox(
+                  height: 160,
+                  child: ScreenTitle(text: 'Ninja Trips'),
+                ),
+                Flexible(
+                  child: TripList(),
+                )
+                //Sandbox(),
+              ],
+            )
+        )
+    );
+  }
+}
